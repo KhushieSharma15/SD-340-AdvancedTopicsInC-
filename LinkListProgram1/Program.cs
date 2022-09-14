fee5cd9e93fd7f264cffb6618a05ddc81c94d6f0
khush <khush@KHUSHI-15>
2022-09-14 13:14:07 -0500

    Add project files.

---
+++ LinkListProgram1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkListProgram1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> MyList = new LinkedList<string>();
+
+            //Add three elements at the start of the list.
+            MyList.AddFirst("ABC");
+            MyList.AddFirst("XYZ");
+            MyList.AddFirst("IJK");
+
+            foreach (string str in MyList)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
